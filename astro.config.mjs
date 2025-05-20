a888025b3d242707d485759c83f40d52569d4b35
Ronja <roba0001@stud.kea.dk>
2025-05-20 21:15:09 +0200

    installeret netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import netlify from '@astrojs/netlify';
+export default {
+  output: 'server',
+  adapter: netlify(),
+};
